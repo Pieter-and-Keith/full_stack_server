c17344ecbd05725cc5ac4fc2633ff779d925d7ba
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-01-29 15:38:11 +1100

    created detail#index and detail#create endpoint in contoller + tested

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -5,5 +5,21 @@ class DetailsController < ApplicationController
         render json: @details
     end
 
+    def create
+        @detail = Detail.create(detail_params)
+        if @detail.errors.any?
+            render json: @joke.errors, status: 422
+        else 
+            render json: @joke, status:201
+        end
+
+    end
+
+    private 
+
+    def detail_params
+        params.require(:detail).permit(:first_name, :last_name, :phone_number, :street_number, :street_name, :suburb, :postcode, :state, :rego, :make, :model)
+    end
+
 
 end

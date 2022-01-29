cb66b6101cc3432ad2f4ccb01e1dc9d817da0b6a
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-01-29 15:10:07 +1100

    created the routes for details

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  scope '/api' do
+    get '/details', to: 'details#index'
+    post '/details', to: 'details#create'
+    get '/details/:id', to: 'details#show'
+    put '/details/:id', to: 'details#update'
+    delete '/details/:id', to: 'details#destroy'
+  end
+
 end

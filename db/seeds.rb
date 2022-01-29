b3cb7cd080775f9c76f38504532c126634ff1669
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-01-29 15:00:38 +1100

    setup of the Details model and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if Detail.count == 0
+    Detail.create(first_name: "Pieter-Jan", last_name:"Delbecke", phone_number: "0405849602", street_number: 10, street_name:"Westminster Avenue",suburb:"Dee Why", postcode: 2099, state:"NSW", rego:"EEH66D", make:"Mazda", model:"2")
+    Detail.create(first_name: "Keith", last_name:"Ching", phone_number: "0499100666", street_number: 53, street_name:"George Street",suburb:"Sydney", postcode: 2000, state:"NSW", rego:"ABC321", make:"Toyota", model:"Hilux")
+end

f6e6797d30050f2dd8ecfb30549e7c2255fe4f35
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-01-30 17:09:45 +1100

    setup of bookings controller #index #show #create + testing

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,39 @@
 class BookingsController < ApplicationController
+    before_action :authenticate_user, only: [:create, :update, :destroy]
+    before_action :set_booking, only: [:show, :update, :destroy]
 
-    # def index
-        
-    # end
+    def index
+        @bookings = Booking.all
+        render json: @bookings
+    end
 
-    # def show
+    def show
+        render json: @booking
+    end
 
-    # end
+    def create
+        @booking = current_user.bookings.create(booking_params)
+        if @booking.errors.any?
+            reneder json: @booking.errors, status: 422
+        else
+            render json: @booking, status: 201
+        end
+    end
+
+
+    private
+
+    def booking_params
+        params.require(:booking).permit(:option_id,:date,:comment, :finished, :paid)
+    end
+
+    def set_booking
+        begin
+            @booking = Booking.find(params[:id])
+        rescue
+            render json: {error: "Booking not found"}, status: 404
+        end
+    end
 
 
 end

42a9782c81fc5689f382799f507fc0da5c24275a
PieterJanDelbecke <pjdelbecke@gmail.com>
2022-02-04 11:09:49 +1100

    issue with details model resoved. yay! :-)

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,31 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :option
+
+  def transform_booking
+    return {
+      user_id: self.user.id,
+      username: self.user.username,
+      email: self.user.email,
+      date: self.date,
+      comment: self.comment,
+      finished: self.finished,
+      paid: self.paid,
+      service_type: self.option.service_type,
+      description: self.option.description,
+      price: self.option.price,
+      first_name: self.user.details[0].first_name,
+      last_name: self.user.details[0].last_name,
+      phone_number: self.user.details[0].phone_number,
+      street_number: self.user.details[0].street_number,
+      street_name: self.user.details[0].street_name,
+      suburb: self.user.details[0].suburb,
+      postcode: self.user.details[0].postcode,
+      state: self.user.details[0].state,
+      rego: self.user.details[0].rego,
+      make: self.user.details[0].make,
+      model: self.user.details[0].model
+    }
+    
+  end
 end
